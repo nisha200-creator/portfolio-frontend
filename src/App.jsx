0c29e3b3e5a516f6cb12899c209d349157683db8
nisha200-creator <nisha70016@gmail.com>
2026-06-06 14:51:31 +0530

    Fix About import case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import About from "./pages/About";
+import About from "./pages/about";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import Skills from "./pages/Skills";
